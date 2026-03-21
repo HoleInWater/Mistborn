e3bf9451b1eb238cb339c5e1594ce4c06c8cabe6
ThenBuzzard100 <thenbuzzard100@gmail.com>
2026-03-21 13:01:58 -0600

    Fix animator hash usage and update walking logic

--- Assets/_Project/Scripts/Animation Scripts/animationStateController.cs
+++ Assets/_Project/Scripts/Animation Scripts/animationStateController.cs
@@ -2,21 +2,34 @@ using UnityEngine;
 
 public class animationStateController : MonoBehaviour
 {
+
     Animator animator;
+    int isWalkingHash;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
         Debug.Log(animator);
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool isWalking = animator.GetBool("isWalkingHash");
+        bool forwardPressed = Input.GetKey("w");
+        
         // If player presses W, the animator starts isWalking
-        if (Input.GetKey("w"))
+        if (!isWalking && forwardPressed)
         {
-            animator.SetBool("isWalking", true);
+            animator.SetBool("isWalkingHash", true);
+        }
+
+        // If player stops pressing W, the animator stops isWalking
+        if (isWalking && !forwardPressed)
+        {
+            animator.SetBool("isWalkingHash", false);
         }
     }
 }
