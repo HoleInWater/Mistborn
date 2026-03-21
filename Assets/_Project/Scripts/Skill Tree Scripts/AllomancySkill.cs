fab05e5485fece771192575e8830a656ba09423b
ThenBuzzard100 <thenbuzzard100@gmail.com>
2026-03-21 13:44:16 -0600

    Refactor AllomancySkill to MagicSkill with attributes

--- Assets/_Project/Scripts/Skill Tree Scripts/AllomancySkill.cs
+++ Assets/_Project/Scripts/Skill Tree Scripts/AllomancySkill.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+// This attribute allows you to create new skill assets via the right-click menu
+[CreateAssetMenu(fileName = "NewMagicSkill", menuName = "Skill Tree/Magic Skill")]
+public class MagicSkill : ScriptableObject 
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("General Info")]
+    public string skillName;
+    public Sprite icon;
+    [TextArea] public string description;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+    [Header("Unlock Requirements")]
+    public int skillPointCost;
+    public List<MagicSkill> prerequisites; // Other skills that must be unlocked first
+
+    [Header("Magic Stats")]
+    public int manaCost;
+    public float cooldown;
+    public float damage;
+
+    [Header("State")]
+    public bool isUnlocked; // Tracks if the player has bought this skill
