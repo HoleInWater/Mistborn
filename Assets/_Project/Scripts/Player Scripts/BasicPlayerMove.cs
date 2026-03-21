1740f895dc3c229b35855452c79e69ed1031319a
ThenBuzzard100 <thenbuzzard100@gmail.com>
2026-03-21 08:54:55 -0600

    Added Basic movement

--- Assets/_Project/Scripts/Player Scripts/BasicPlayerMove.cs
+++ Assets/_Project/Scripts/Player Scripts/BasicPlayerMove.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+    public float sensitivity = 100f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // WASD Movement
+        float x = Input.GetAxis("Horizontal"); // A, D
+        float z = Input.GetAxis("Vertical");   // W, S
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        transform.position += move * moveSpeed * Time.deltaTime;
+
+        // Arrow Key Looking (Rotation)
+        float rotateX = 0;
+        if (Input.GetKey(KeyCode.LeftArrow)) rotateX = -1;
+        if (Input.GetKey(KeyCode.RightArrow)) rotateX = 1;
+
+        transform.Rotate(Vector3.up * rotateX * sensitivity * Time.deltaTime);
     }
 }
