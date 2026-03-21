e0cb0564bf8033abaa5875790ca951b9218d9d8e
HoleInWater <20landonadams09@gmail.com>
2026-03-21 17:25:52 -0600

    Add movement abilities - DodgeRoll, WallRun, MetalMagnet, StealthSystem

---
+++ Assets/_Project/Scripts/Player/StealthSystem.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+
+public class StealthSystem : MonoBehaviour
+{
+    [Header("Stealth Settings")]
+    public float crouchSpeed = 2f;
+    public float normalSpeed = 5f;
+    public float crouchHeight = 1f;
+    public float normalHeight = 2f;
+    
+    [Header("References")]
+    public PlayerMove playerController;
+    public CapsuleCollider playerCollider;
+    public Camera playerCamera;
+    
+    private bool isCrouching = false;
+    private float originalHeight;
+    private float originalCenter;
+    
+    void Start()
+    {
+        if (playerCollider != null)
+        {
+            originalHeight = playerCollider.height;
+            originalCenter = playerCollider.center.y;
+        }
+        
+        if (playerCamera != null)
+        {
+            originalHeight = playerCamera.transform.localPosition.y;
+        }
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            ToggleCrouch();
+        }
+    }
+    
+    void ToggleCrouch()
+    {
+        isCrouching = !isCrouching;
+        
+        if (isCrouching)
+        {
+            Crouch();
+        }
+        else
+        {
+            StandUp();
+        }
+    }
+    
+    void Crouch()
+    {
+        if (playerCollider != null)
+        {
+            playerCollider.height = crouchHeight;
+            playerCollider.center = new Vector3(0, crouchHeight / 2f, 0);
+        }
+        
+        if (playerController != null)
+        {
+            playerController.moveSpeed = crouchSpeed;
+        }
+        
+        Debug.Log("Crouching");
+    }
+    
+    void StandUp()
+    {
+        if (playerCollider != null)
+        {
+            playerCollider.height = originalHeight;
+            playerCollider.center = new Vector3(0, originalCenter, 0);
+        }
+        
+        if (playerController != null)
+        {
+            playerController.moveSpeed = normalSpeed;
+        }
+        
+        Debug.Log("Standing");
+    }
+    
+    public bool IsCrouching()
+    {
+        return isCrouching;
+    }
+}
