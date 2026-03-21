6f55c444951fc2563bc6f0a990257f636a525b54
ThenBuzzard100 <thenbuzzard100@gmail.com>
2026-03-21 13:03:09 -0600

    Fix animator parameter name for walking state

--- Assets/_Project/Scripts/Animation Scripts/animationStateController.cs
+++ Assets/_Project/Scripts/Animation Scripts/animationStateController.cs
@@ -4,32 +4,30 @@ public class animationStateController : MonoBehaviour
 {
 
     Animator animator;
-    int isWalkingHash;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animator = GetComponent<Animator>();
-        isWalkingHash = Animator.StringToHash("isWalking");
         Debug.Log(animator);
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool isWalking = animator.GetBool("isWalkingHash");
+        bool isWalking = animator.GetBool("isWalking");
         bool forwardPressed = Input.GetKey("w");
         
         // If player presses W, the animator starts isWalking
         if (!isWalking && forwardPressed)
         {
-            animator.SetBool("isWalkingHash", true);
+            animator.SetBool("isWalking", true);
         }
 
         // If player stops pressing W, the animator stops isWalking
         if (isWalking && !forwardPressed)
         {
-            animator.SetBool("isWalkingHash", false);
+            animator.SetBool("isWalking", false);
         }
     }
 }
