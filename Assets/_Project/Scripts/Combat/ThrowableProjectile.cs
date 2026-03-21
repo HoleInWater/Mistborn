87ecd0c297e0d89ed4f96ed962513c244929dd0d
HoleInWater <20landonadams09@gmail.com>
2026-03-21 17:28:36 -0600

    Add world scripts - LootDrop, Door, Inventory, ThrowableProjectile

---
+++ Assets/_Project/Scripts/Combat/ThrowableProjectile.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class ThrowableProjectile : MonoBehaviour
+{
+    [Header("Projectile Settings")]
+    public float speed = 20f;
+    public float damage = 10f;
+    public float lifetime = 5f;
+    public GameObject impactEffect;
+    
+    [Header("Physics")]
+    public Rigidbody rb;
+    
+    void Start()
+    {
+        if (rb == null)
+            rb = GetComponent<Rigidbody>();
+        
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, lifetime);
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            EnemyHealth enemy = other.GetComponent<EnemyHealth>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+            
+            SpawnImpactEffect();
+            Destroy(gameObject);
+        }
+    }
+    
+    void SpawnImpactEffect()
+    {
+        if (impactEffect != null)
+        {
+            Instantiate(impactEffect, transform.position, Quaternion.identity);
+        }
+    }
+}
