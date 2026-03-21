0621c03d54a98d9c61cba3ad41fd0509e3bd10c7
ThenBuzzard100 <thenbuzzard100@gmail.com>
2026-03-21 12:49:13 -0600

    Refactor animationStateController class and methods
    Renamed class to 'animationStateController' and fixed method calls.

--- Assets/_Project/Scripts/Animation Scripts/animationStateController.cs
+++ Assets/_Project/Scripts/Animation Scripts/animationStateController.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class animationStateController : MonoBehaviour
 {
+    Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        Debug.Log(animator);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // If player presses W, the animator starts isWalking
+        if (Input.Getkey("w"))
+        {
+            animator.setBool("isWalking", true);
+        ]
     }
 }
