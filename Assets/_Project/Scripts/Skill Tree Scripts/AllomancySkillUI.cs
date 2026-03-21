58e0ad6c8eb6703f11407f161cdbb0af862d8c05
ThenBuzzard100 <thenbuzzard100@gmail.com>
2026-03-21 15:22:28 -0600

    Rename class and update skill array for Allomancy

--- Assets/_Project/Scripts/Skill Tree Scripts/AllomancySkillUI.cs
+++ Assets/_Project/Scripts/Skill Tree Scripts/AllomancySkillUI.cs
@@ -1,18 +1,18 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class MagicTreeUI : MonoBehaviour
+public class AllomancyTreeUI : MonoBehaviour
 {
     public VisualTreeAsset nodeTemplate; // Drag your SkillNode UXML here
-    public MagicSkill[] allMagicSkills;   // Drag your Skill assets here
+    public AllomancySkill[] allAllomancySkills;   // Drag your Skill assets here
 
     void OnEnable()
     {
         // Get the root of your UI Document
         var root = GetComponent<UIDocument>().rootVisualElement;
-        var container = root.Q<VisualElement>("MagicTabContent"); // Name must match UI Builder
+        var container = root.Q<VisualElement>("AllomancyTabContent"); // Name must match UI Builder
 
-        foreach (var skill in allMagicSkills)
+        foreach (var skill in allAllomancySkills)
         {
             // Instantiate (Clone) the template
             VisualElement node = nodeTemplate.Instantiate();
