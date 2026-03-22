a7905394c422b8716f16803befdc5b24840d8b82
ThenBuzzard100 <thenbuzzard100@gmail.com>
2026-03-22 17:39:40 -0600

    Update animationStateController.cs

--- Assets/_Project/Scripts/Animation Scripts/animationStateController.cs
+++ Assets/_Project/Scripts/Animation Scripts/animationStateController.cs
@@ -4,59 +4,46 @@ using UnityEngine;
 public class AnimationStateController : MonoBehaviour
 {
     public Transform groundCheck;
+    public float groundDistance = 0.4f; // The size of the "check bubble"
+    public LayerMask groundMask;      // Set this to the "Ground" layer in the Inspector
 
-    // NOTE: Consider adding [SerializeField] for animator field
     Animator animator;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    bool isGrounded;
+
     void Start()
     {
         animator = GetComponent<Animator>();
-        Debug.Log(animator);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // 1. CONSTANTLY CHECK IF ON THE FLOOR
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
         bool isRunning = animator.GetBool("isRunning");
         bool isWalking = animator.GetBool("isWalking");
         bool isJumping = animator.GetBool("isJumping");
+
         bool forwardPressed = Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d");
         bool runPressed = Input.GetKey("left shift");
         bool jumpPressed = Input.GetKey("space");
         
-        // If player presses W, the animator starts isWalking
-        if (!isWalking && forwardPressed)
-        {
-            animator.SetBool("isWalking", true);
-        }
+        // WALKING LOGIC
+        if (!isWalking && forwardPressed) animator.SetBool("isWalking", true);
+        if (isWalking && !forwardPressed) animator.SetBool("isWalking", false);
 
-        // If player stops pressing W, the animator stops isWalking
-        if (isWalking && !forwardPressed)
-        {
-            animator.SetBool("isWalking", false);
-        }
+        // RUNNING LOGIC
+        if (!isRunning && (forwardPressed && runPressed)) animator.SetBool("isRunning", true);
+        if (isRunning && (!forwardPressed || !runPressed)) animator.SetBool("isRunning", false);
 
-        //If player is holding left shift and w then run
-        if (!isRunning && (forwardPressed && runPressed))
-        {
-            animator.SetBool("isRunning", true);
-        }
-        
-        //If player is not holding left shift and w then don't run
-        if (isRunning && (!forwardPressed || !runPressed))
-        {
-            animator.SetBool("isRunning", false);
-        }
-
-        //If player is idle, then you can jump
-        if (!isJumping && (!forwardPressed && jumpPressed))
+        // JUMP START: If grounded and space is pressed, start jump
+        if (!isJumping && isGrounded && jumpPressed)
         {
             animator.SetBool("isJumping", true);
         }
 
-        //If player is not idle, then you can't jump
-        if (isJumping && (forwardPressed && !jumpPressed))
+        // JUMP END: If we were jumping but are now touching the ground, STOP
+        if (isJumping && isGrounded)
         {
             animator.SetBool("isJumping", false);
         }
