cf4fb1c2636eaf849540e07012be58768a98e9b9
HoleInWater <20landonadams09@gmail.com>
2026-03-22 16:41:54 -0600

    Add Tooltip attribute suggestions for MetalHUD.cs

--- Assets/_Project/Scripts/GUI/MetalHUD.cs
+++ Assets/_Project/Scripts/GUI/MetalHUD.cs
@@ -4,9 +4,13 @@ using UnityEngine.UI;
 public class MetalHUD : MonoBehaviour
 {
     [Header("Metal Display")]
+    // NOTE: Consider adding [Tooltip("Text component to display current metal name")] attribute
     public Text currentMetalText;
+    // NOTE: Consider adding [Tooltip("Image component for metal icon")] attribute
     public Image metalIcon;
+    // NOTE: Consider adding [Tooltip("Slider showing metal reserve amount")] attribute
     public Slider metalReserveSlider;
+    // NOTE: Consider adding [Tooltip("Array of GameObjects for metal type indicators")] attribute
     public GameObject[] metalIndicators;
     
     private AllomancySkill.MetalType currentMetal = AllomancySkill.MetalType.Steel;
