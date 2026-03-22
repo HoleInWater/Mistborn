966804a7a72a0327d7fbb9da1b83453f391c226a
HoleInWater <20landonadams09@gmail.com>
2026-03-21 18:27:54 -0600

    Add systems: EffectSpawner, GameState, Singleton, ObjectPool
    - EffectSpawner: Centralized VFX spawning
    - GameState: Simple game state machine
    - Singleton: Base class for singletons
    - ObjectPool: Object pooling for performance

---
+++ Assets/_Project/Scripts/Effects/EffectSpawner.cs
@@ -0,0 +1,77 @@
+/// <summary>
+/// Simple particle effect spawner.
+/// Usage: EffectSpawner.Spawn("metal_pickup", position);
+/// </summary>
+public class EffectSpawner : MonoBehaviour
+{
+    public static EffectSpawner Instance { get; private set; }
+    
+    // EFFECT PREFABS - Assign in Inspector
+    public GameObject metalPickupEffect;
+    public GameObject metalPushEffect;
+    public GameObject metalPullEffect;
+    public GameObject dashEffect;
+    public GameObject rollEffect;
+    public GameObject damageEffect;
+    public GameObject healEffect;
+    public GameObject checkpointEffect;
+    
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    
+    // Spawn by name
+    public static void Spawn(string effectName, Vector3 position, Quaternion rotation = default)
+    {
+        if (Instance == null) return;
+        
+        GameObject prefab = null;
+        
+        switch (effectName.ToLower())
+        {
+            case "metal_pickup": prefab = Instance.metalPickupEffect; break;
+            case "metal_push": prefab = Instance.metalPushEffect; break;
+            case "metal_pull": prefab = Instance.metalPullEffect; break;
+            case "dash": prefab = Instance.dashEffect; break;
+            case "roll": prefab = Instance.rollEffect; break;
+            case "damage": prefab = Instance.damageEffect; break;
+            case "heal": prefab = Instance.healEffect; break;
+            case "checkpoint": prefab = Instance.checkpointEffect; break;
+        }
+        
+        if (prefab != null)
+        {
+            Instantiate(prefab, position, rotation);
+        }
+    }
+    
+    // Specific spawn methods
+    public static void MetalPickup(Vector3 position)
+    {
+        Spawn("metal_pickup", position);
+    }
+    
+    public static void Dash(Vector3 position)
+    {
+        Spawn("dash", position);
+    }
+    
+    public static void Damage(Vector3 position)
+    {
+        Spawn("damage", position);
+    }
+    
+    public static void Heal(Vector3 position)
+    {
+        Spawn("heal", position);
+    }
+    
+    public static void Checkpoint(Vector3 position)
+    {
+        Spawn("checkpoint", position);
+    }
+}
---
+++ Assets/_Project/Scripts/Systems/ObjectPool.cs
@@ -0,0 +1,93 @@
+/// <summary>
+/// Simple object pool for reusing GameObjects.
+/// Usage: ObjectPool.Instance.Get("coin", position);
+/// </summary>
+public class ObjectPool : MonoBehaviour
+{
+    public static ObjectPool Instance { get; private set; }
+    
+    // POOLS - Assign prefabs in Inspector
+    public GameObject[] pooledPrefabs;
+    public int[] poolSizes;
+    
+    // INTERNAL
+    private System.Collections.Generic.Dictionary<string, System.Collections.Generic.Queue<GameObject>> pools;
+    
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            InitializePools();
+        }
+    }
+    
+    void InitializePools()
+    {
+        pools = new System.Collections.Generic.Dictionary<string, System.Collections.Generic.Queue<GameObject>>();
+        
+        for (int i = 0; i < pooledPrefabs.Length; i++)
+        {
+            GameObject prefab = pooledPrefabs[i];
+            int size = poolSizes.Length > i ? poolSizes[i] : 10;
+            string name = prefab.name;
+            
+            Queue<GameObject> queue = new System.Collections.Generic.Queue<GameObject>();
+            
+            for (int j = 0; j < size; j++)
+            {
+                GameObject obj = Instantiate(prefab, transform);
+                obj.SetActive(false);
+                queue.Enqueue(obj);
+            }
+            
+            pools.Add(name, queue);
+        }
+    }
+    
+    // Get object from pool
+    public GameObject Get(string poolName, Vector3 position, Quaternion rotation = default)
+    {
+        if (!pools.ContainsKey(poolName))
+        {
+            Debug.LogWarning($"Pool '{poolName}' not found!");
+            return null;
+        }
+        
+        Queue<GameObject> pool = pools[poolName];
+        
+        // Try to find inactive object
+        while (pool.Count > 0)
+        {
+            GameObject obj = pool.Dequeue();
+            
+            if (obj != null && !obj.activeSelf)
+            {
+                obj.transform.position = position;
+                obj.transform.rotation = rotation;
+                obj.SetActive(true);
+                return obj;
+            }
+        }
+        
+        // No inactive objects - instantiate new
+        GameObject prefab = System.Array.Find(pooledPrefabs, p => p.name == poolName);
+        if (prefab != null)
+        {
+            GameObject newObj = Instantiate(prefab, position, rotation, transform);
+            return newObj;
+        }
+        
+        return null;
+    }
+    
+    // Return object to pool
+    public void Return(string poolName, GameObject obj)
+    {
+        if (pools.ContainsKey(poolName))
+        {
+            obj.SetActive(false);
+            pools[poolName].Enqueue(obj);
+        }
+    }
+}
