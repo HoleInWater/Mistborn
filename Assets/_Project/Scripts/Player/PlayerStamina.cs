df1db424c31ddd7a27c4b412464bd9c44a98632c
ThenBuzzard100 <thenbuzzard100@gmail.com>
2026-03-22 12:34:24 -0600

    Fixes

--- Assets/_Project/Scripts/Player/PlayerStamina.cs
+++ Assets/_Project/Scripts/Player/PlayerStamina.cs
@@ -1,69 +1,16 @@
 using UnityEngine;
 
-public class PlayerStamina : MonoBehaviour
+public class PlayerStamina1 : MonoBehaviour
 {
-    [Header("Stamina Settings")]
-    public float maxStamina = 100f;
-    public float currentStamina = 100f;
-    public float sprintDrainRate = 10f;
-    public float recoveryRate = 5f;
-    public float minStaminaToSprint = 10f;
-    
-    [Header("References")]
-    public PlayerMove playerController;
-    
-    private bool isSprinting = false;
-    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftShift) && currentStamina > minStaminaToSprint)
-        {
-            DrainStamina();
-        }
-        else
-        {
-            RecoverStamina();
-        }
-    }
-    
-    void DrainStamina()
-    {
-        currentStamina -= sprintDrainRate * Time.deltaTime;
-        currentStamina = Mathf.Max(0, currentStamina);
         
-        if (playerController != null)
-        {
-            playerController.moveSpeed = playerController.moveSpeed * 1.5f;
-        }
-        
-        isSprinting = true;
-    }
-    
-    void RecoverStamina()
-    {
-        if (isSprinting && currentStamina < minStaminaToSprint)
-        {
-            Debug.Log("Too tired to sprint!");
-        }
-        
-        currentStamina += recoveryRate * Time.deltaTime;
-        currentStamina = Mathf.Min(currentStamina, maxStamina);
-        
-        if (playerController != null && isSprinting)
-        {
-            playerController.moveSpeed = playerController.moveSpeed / 1.5f;
-        }
-        
-        isSprinting = false;
-    }
-    
-    public float GetStaminaPercentage()
-    {
-        return currentStamina / maxStamina;
-    }
-    
-    public bool CanSprint()
-    {
-        return currentStamina > minStaminaToSprint;
     }
 }
