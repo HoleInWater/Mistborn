fffb37e505c30f8bcd0016c774b6d3c09cc7354f
ThenBuzzard100 <thenbuzzard100@gmail.com>
2026-03-23 14:01:58 -0600

    Update animationStateController.cs

--- Assets/_Project/Scripts/Animation Scripts/animationStateController.cs
+++ Assets/_Project/Scripts/Animation Scripts/animationStateController.cs
@@ -70,7 +70,7 @@ public class AnimationStateController : MonoBehaviour
         }
 
         // Don't jump while not running
-        if (isRunJump && (!jumpPressed && (!forwardPressed || !runPressed)))
+        if (isRunJump && (!jumpPressed && (forwardPressed || !runPressed)))
         {
             animator.SetBool("isRunJump", false);
         }
