4859ca57ddc8f16678a7a3718151efa8bbc3dde9
ThenBuzzard100 <thenbuzzard100@gmail.com>
2026-03-22 18:10:44 -0600

    Update AllomanticSight.cs

--- Assets/_Project/Scripts/Allomancy/AllomanticSight.cs
+++ Assets/_Project/Scripts/Allomancy/AllomanticSight.cs
@@ -4,6 +4,7 @@
 // This ability reveals all metal within range, passing through walls and geometry.
 
 using UnityEngine;
+using System.Collections
 using System.Collections.Generic;
 
 public class AllomanticSight : MonoBehaviour
